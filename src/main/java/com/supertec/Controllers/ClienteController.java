4b76a2f34dceac4d676e87fa53ec224c5f8f4a3a
blackharts <35812381+blackharts@users.noreply.github.com>
2018-12-06 00:17:17 -0300

    Guardado-Cliente
    se ha realizado el metodo de guardar con algunos errores, en jpa

--- src/main/java/com/supertec/Controllers/ClienteController.java
+++ src/main/java/com/supertec/Controllers/ClienteController.java
@@ -1,44 +1,67 @@
 package com.supertec.Controllers;
 
+import static com.sun.org.apache.xalan.internal.lib.ExsltDatetime.date;
 import com.supertec.Clases.Cliente;
 import com.supertec.JpaController.ClienteJpaController;
+import java.util.Date;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class ClienteController {
-
-@Autowired ClienteJpaController cliJpa;
-  
+    
     @RequestMapping("/cliente")
     public String showCliente(Model model, @ModelAttribute("result") String result) {
         Cliente cliente = new Cliente();
-        model.addAttribute("cliente", cliente );
-          model.addAttribute("result", result );
+        model.addAttribute("cliente", cliente);
+        model.addAttribute("result", result);
         return "cliente";
     }
-     @RequestMapping(value="/cliente/save",method=RequestMethod.POST)
-    public String handleCliente(@ModelAttribute("cliente") Cliente clienteForm,Model model,
+    
+    @RequestMapping(value = "/cliente/save", method = RequestMethod.POST)
+    public String handleCliente(@ModelAttribute("cliente") Cliente clienteForm, Model model,
             RedirectAttributes red) {
-        red.addFlashAttribute("result","Se ha registrado Exitosamente!");
-       
+        red.addFlashAttribute("result", "Se ha registrado Exitosamente!");
+        
         return "redirect:/index";
     }
-     @RequestMapping("/cliente/{id}/update")//metodo para editar
- 	public String showUpdate(Model model,
- 			@PathVariable("id") int id) {
- 		
- 		
- 		Cliente cliente = cliJpa.findCliente(id);
- 		model.addAttribute("cliente", cliente);
- 		
- 		return "cliente";
- 	}
+    @RequestMapping(value="/cliente/request",method = RequestMethod.POST)
+    public String handleSave(
+            @RequestParam("nombre") String nombre,
+            @RequestParam("usuario") String usuario,
+            @RequestParam("rut") String rut,
+            @RequestParam("correo") String correo,
+            @RequestParam("telefono") String telefono,
+            @RequestParam("contrasenia") String contrasenia,
+            @RequestParam("fechaNacimiento") Date fechaNacimiento,
+            Model model) throws Exception {
+        
+        if (nombre.trim().equals("")) {
+            return "error500";
+        } else {
+            
+            Cliente cli = new Cliente();
+            cli.setNombre(nombre);
+            cli.setRut(rut);
+            cli.setUsuario(usuario);
+            cli.setCorreo(correo);
+            cli.setContrasenia(contrasenia);
+            cli.setFechaNacimiento(fechaNacimiento);
+            cli.setTelefono(telefono);
+            
+            ClienteJpaController cl = new ClienteJpaController();
+            
+            cl.create(cli);
+            
+            model.addAttribute("cliente", cl);
+            return "index";
+        }
+        
+    }
 }
